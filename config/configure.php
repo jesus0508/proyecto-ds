9e3085a50ab211809a758e12bc3f07cbcdb84634
Jesus Alarcon <jesus.alarcon3@unmsm.edu.pe>
2018-11-21 20:10:38 -0500

    Subiendo a heroku

--- config/configure.php
+++ config/configure.php
@@ -1,6 +1,6 @@
 <?php
     define('RUTA_APP',dirname(dirname(__FILE__)));
     //define('RUTA_URL',RUTA_APP);
-    define('RUTA_URL','http://localhost:8080/Proyecto-Rokys-DSistemas/');
-    //define('RUTA_URL','https://mighty-ocean-96087.herokuapp.com/');
+    //define('RUTA_URL','http://localhost:8080/Proyecto-Rokys-DSistemas/');
+    define('RUTA_URL','https://mighty-ocean-96087.herokuapp.com/');
 ?>
